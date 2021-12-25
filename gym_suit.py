c378fadf9c9704f702eb35d79a8133571153c94c
heewoneha <jhjmo0719h@naver.com>
2021-12-25 13:14:26 +0900

    day 3

---
+++ gym_suit.py
@@ -0,0 +1,26 @@
+def solution(n, lost, reserve): #x번째-1 == 인덱스
+    answer = 0
+    
+    dress = []
+    for i in range(0, n):
+        dress.append(1)
+        
+        if ((i+1) in reserve):
+            dress[i] += 1
+            
+        if ((i+1) in lost):
+            dress[i] -= 1
+
+    for j in range(0, n):
+        if (dress[j] == 0):
+            if ((j-1) > -1 and (dress[j-1] == 2)):
+                dress[j-1] -= 1
+                dress[j] += 1
+            elif ((j+1) < len(dress) and (dress[j+1] == 2)):
+                dress[j+1] -= 1
+                dress[j] += 1
+    
+    answer += dress.count(1)
+    answer += dress.count(2)
+    
+    return answer
---
+++ k_value.py
@@ -0,0 +1,21 @@
+def solution(array, commands):
+    answer = []
+    
+    for i in range(0, len(commands)):
+        what = commands[i]
+
+        arr = []
+
+        x = commands[i][0] - 1
+        y = commands[i][1] - 1
+        for a in range(x, y + 1):
+            arr.append(array[a])
+        
+        arr.sort()
+        
+        x = commands[i][2] - 1
+        answer.append(arr[x])
+    
+    return answer
+
+solution([1,5,2,6,3,7,4], [[2,5,3],[4,4,1],[1,7,3]])
