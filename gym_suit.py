4a52bcaf5d11bf9e411515f7125777e296c522c6
heewoneha <jhjmo0719h@naver.com>
2022-02-01 10:39:58 +0000

    체육복(그리디)

--- gym_suit.py
+++ gym_suit.py
@@ -4,10 +4,8 @@ def solution(n, lost, reserve): #x번째-1 == 인덱스
     dress = []
     for i in range(0, n):
         dress.append(1)
-        
         if ((i+1) in reserve):
             dress[i] += 1
-            
         if ((i+1) in lost):
             dress[i] -= 1
 
